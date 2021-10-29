4f54850a28780667d21fb397c62b64519eded8a2
nicoezdias <nicolasezdias@gmail.com>
2021-10-29 17:02:15 -0300

    Boton cerrar sesion

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,62 +1,102 @@
 window.addEventListener('load', () => {
+	/* -------------------------------- Variables ------------------------------- */
+	const pUserInfo = document.querySelector('.user-info p');
+	const urlApi = 'https://ctd-todo-api.herokuapp.com/v1';
+	const jwt = localStorage.getItem('token');
 	const formulario = document.querySelector('form');
-	const nuevaTareaInfo = document.querySelector('#nuevaTarea');
+	const inputNuevaTarea = document.querySelector('#nuevaTarea');
+	const tareasPendientes = document.querySelector('.tareas-pendientes');
+	const tareasTerminadas = document.querySelector('.tareas-terminadas');
+	const cerrarSesion = document.querySelector('#closeApp');
+	/* -------------------------------------------------------------------------- */
 
-	obtenerUsuario('https://ctd-todo-api.herokuapp.com/v1/users/getMe');
-	obtenerTareas('https://ctd-todo-api.herokuapp.com/v1/tasks');
+	obtenerNombreUsuario(`${urlApi}/users/getMe`, jwt);
+	obtenerlistaTareas(`${urlApi}/tasks`, jwt);
 
 	formulario.addEventListener('submit', function (evento) {
 		evento.preventDefault();
-		const datos = {
-			description: nuevaTareaInfo.value,
+		const nuevaTareaInfo = {
+			description: inputNuevaTarea.value,
 			completed: false,
 		};
-		crearTarea('https://ctd-todo-api.herokuapp.com/v1/tasks', datos);
+		crearNuevaTarea(`${urlApi}/tasks`, jwt, nuevaTareaInfo);
+		formulario.reset();
 	});
+
+	cerrarSesion.addEventListener('click', function () {
+		const respuesta = confirm('¿Desea cerrar sesión?');
+		if (respuesta) {
+			location.href = './index.html';
+			localStorage.removeItem('token');
+		}
+	});
+	/* -------------------------------- Funciones ------------------------------- */
+	function obtenerNombreUsuario(url, token) {
+		const settings = {
+			method: 'GET',
+			headers: {
+				authorization: token,
+			},
+		};
+		fetch(url, settings)
+			.then((response) => response.json())
+			.then((data) => {
+				pUserInfo.innerHTML = data.firstName + ' ' + data.lastName;
+			});
+	}
+	function obtenerlistaTareas(url, token) {
+		const settings = {
+			method: 'GET',
+			headers: {
+				authorization: token,
+			},
+		};
+		fetch(url, settings)
+			.then((response) => response.json())
+			.then((data) => {
+				renderizarInfo(data);
+			});
+	}
+	function crearNuevaTarea(url, token, payload) {
+		const settings = {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+				authorization: token,
+			},
+			body: JSON.stringify(payload),
+		};
+		fetch(url, settings)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+			});
+	}
+	function renderizarInfo(listado) {
+		listado.forEach((tarea) => {
+			if (tarea.completed) {
+				tareasTerminadas.innerHTML += `<li class="tarea">
+				<div class="done"></div>
+				<div class="descripcion">
+				<p class="nombre">${tarea.description}</p>
+				<div>
+				<button><i id="${tarea.id}" class="fas
+				fa-undo-alt change"></i></button>
+				<button><i id="${tarea.id}" class="far
+				fa-trash-alt"></i></button>
+				</div>
+				</div>
+				</li>`;
+			} else {
+				tareasPendientes.innerHTML += `<li class="tarea">
+				<div class="not-done change" id="${tarea.id}"></div>
+				<div class="descripcion">
+				<p class="nombre">${tarea.description}</p>
+                <p class="timestamp"><i class="farfa-calendar-alt"></i> ${tarea.createdAt}</p>
+				</div> 
+				</li>`;
+			}
+		});
+	}
+	/* -------------------------------------------------------------------------- */
 });
-
-function obtenerUsuario(urlApi) {
-	const pUserInfo = document.querySelector('.user-info p');
-	const settings = {
-		method: 'GET',
-		headers: {
-			authorization: localStorage.getItem('token'),
-		},
-	};
-	fetch(urlApi, settings)
-		.then((response) => response.json())
-		.then((data) => {
-			pUserInfo.innerHTML = data.firstName + ' ' + data.lastName;
-		});
-}
-
-function obtenerTareas(urlApi) {
-	const settings = {
-		method: 'GET',
-		headers: {
-			authorization: localStorage.getItem('token'),
-		},
-	};
-	fetch(urlApi, settings)
-		.then((response) => response.json())
-		.then((data) => {
-			console.log(data);
-		});
-}
-function crearTarea(urlApi, nuevosDatos) {
-	const payload = JSON.stringify(nuevosDatos);
-	console.log(payload);
-	const settings = {
-		method: 'POST',
-		headers: {
-			'Content-type': 'application/json',
-			authorization: localStorage.getItem('token'),
-		},
-		body: payload,
-	};
-	fetch(urlApi, settings)
-		.then((response) => response.json())
-		.then((data) => {
-			console.log(data);
-		});
-}
